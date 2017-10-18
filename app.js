c2a3573c13bac9e46e461db32d592bb9d124af7b
awwa500@gmail.com <awwa500@gmail.com>
2017-10-18 22:38:34 +0900

    default port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
