02f725a8e82a031bf11353fe10e782df72591976
awwa500@gmail.com <awwa500@gmail.com>
2017-10-20 23:13:14 +0900

    enable cors headers

--- app.js
+++ app.js
@@ -19,16 +19,15 @@ app.use(morgan('dev', {immediate: true}));
 //     'HEAD',
 //     'OPTIONS'
 // ];
-// app.use(function(req, res, next) {
-// 	console.log('origin: ' + req.headers.origin);
-// 	res.header('Access-Control-Allow-Origin', req.headers.origin);
-// 	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-// 	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-// 	res.header('Access-Control-Allow-Credentials', true);
-// 	res.header('Access-Control-Max-Age', '86400');
-//
-//   next();
-// });
+app.use(function(req, res, next) {
+	console.log('origin: ' + req.headers.origin);
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Max-Age', '86400');
+  next();
+});
 var game = require('./models/game.js');
 
 // app.options('*', function (req, res) {
